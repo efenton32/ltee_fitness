808b4be98b8c3455f2d12a31b67fa3901172cc80
efenton32 <eliotfenton@gmail.com>
2022-09-01 11:15:29 -0400

    Updated fastq_to_counts

--- initial_files/fastq_to_counts.sh
+++ initial_files/fastq_to_counts.sh
@@ -1,11 +1,11 @@
-paste <(zcat fastq/TJ/1H6_S90_R1_001.fastq.gz) <(zcat fastq/TJ/1H6_S90_R2_001.fastq.gz) |
-awk -F"TAGGAGG" '
-/......TGTGGG/ && /[ATGC].....AGTAGG/ && /TAGGAGG/ && /CCACGGC/{a[substr($1,1,6) substr($1,28,6) substr($2,1,26)]++ }
+paste <(zcat fastq/TJ/1G6_S78_R1_001.fastq.gz) <(zcat fastq/TJ/1G6_S78_R2_001.fastq.gz) |
+awk '
+/......TGTGGG/ && /[ATGC].....AGTAGG/ && /TAGGAGG/ && /CCACGGC/{
+    a[substr($1,1,6) substr($2,1,6) substr($2,index($2,"TAGGAGG")+7,26)]++
+}
 
 END {
-    for (i in a) {
-        print i " " a[i]
-    }
+    for (i in a) {print i " " a[i]}
 }
 
 ' > .holder.txt
@@ -16,9 +16,7 @@ awk '
 
 END {
     for (i in a) {
-        if (a[i] > 1000) {
-            print i " " a[i]
-        }
+        if (a[i] > 1000) {print i " " a[i]}
     }
 }
 ' > test.txt
