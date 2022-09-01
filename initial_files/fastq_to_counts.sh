9fd75d24999a37fd49102e86e06b88da5ef68081
efenton32 <eliotfenton@gmail.com>
2022-09-01 14:24:42 -0400

    Improved fastq -> count file pipeline

--- initial_files/fastq_to_counts.sh
+++ initial_files/fastq_to_counts.sh
@@ -1,25 +1,14 @@
-paste <(zcat fastq/TJ/1G6_S78_R1_001.fastq.gz) <(zcat fastq/TJ/1G6_S78_R2_001.fastq.gz) |
+paste <(zcat fastq/TJ/$1_R1_001.fastq.gz) <(zcat fastq/TJ/$1_R2_001.fastq.gz) |
 awk '
 /......TGTGGG/ && /[ATGC].....AGTAGG/ && /TAGGAGG/ && /CCACGGC/{
     a[substr($1,1,6) substr($2,1,6) substr($2,index($2,"TAGGAGG")+7,26)]++
 }
 
 END {
-    for (i in a) {print i " " a[i]}
-}
-
-' > .holder.txt
-
-cat .holder.txt |
-awk '
-{a[substr($1,15,26)]++}
-
-END {
-    for (i in a) {
-        if (a[i] > 1000) {print i " " a[i]}
+    for (i in a) {b[substr(i,15,26)]++}
+    for (i in b) {
+        if (b[i] > 1000) {print i " " b[i]}
     }
 }
-' > test.txt
-
-rm .holder.txt
+' > count_files/$1.txt
 
