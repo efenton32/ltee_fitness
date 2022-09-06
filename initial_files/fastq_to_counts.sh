bbc93a2bce50354feac53481cc14b7e7cf7d0e2d
efenton32 <eliotfenton@gmail.com>
2022-09-06 12:29:19 -0400

    Finished fastq -> count file pipeline

--- initial_files/fastq_to_counts.sh
+++ initial_files/fastq_to_counts.sh
@@ -1,14 +1,28 @@
-paste <(zcat fastq/TJ/$1_R1_001.fastq.gz) <(zcat fastq/TJ/$1_R2_001.fastq.gz) |
 awk '
-/......TGTGGG/ && /[ATGC].....AGTAGG/ && /TAGGAGG/ && /CCACGGC/{
+function all_SNPs(sequence){
+    regex = sequence
+    for (i=1; i<=length(sequence); i++)
+        regex = regex "|" substr(sequence,1,i-1) "." substr(sequence,i+1)
+    return regex
+
+}
+
+(NR==FNR) {bcs[$2] = all_SNPs($2)}
+
+/......TGTGGG/ && /[ATGC].....AGTAGG/ && /TAGGAGG/ && /CCACGGC/(NR>FNR){
     a[substr($1,1,6) substr($2,1,6) substr($2,index($2,"TAGGAGG")+7,26)]++
 }
 
 END {
-    for (i in a) {b[substr(i,15,26)]++}
+    for (i in a) {b[substr(i,13,26)]++}
     for (i in b) {
-        if (b[i] > 1000) {print i " " b[i]}
+        for (j in bcs) {
+            if (i ~ bcs[j]) {counts[j]+=b[i]}
+        }
     }
+    for (i in counts) {print i " " counts[i]}
 }
-' > count_files/$1.txt
+
+
+' barcodes.txt <(paste <(zcat fastq/TJ/$1_R1_001.fastq.gz) <(zcat fastq/TJ/$1_R2_001.fastq.gz)) > count_files/$1.txt
 
